6c7811ddc4a25be1acac4654762244843ef7a1c8
nouraagamal <138740857+nouraagamal@users.noreply.github.com>
2024-09-04 17:53:44 +0300

    Create project.py

---
+++ project.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import subprocess
+
+skills_app = Flask(__name__)
+
+@skills_app.route("/")
+def homepage():
+    return render_template("index.html",
+                           pagetitle="HomePage",
+                           head="Welcome to the Maze!",
+                           description="This is a monsters maze where you can enjoy playing")
+
+@skills_app.route("/start_game", methods=["POST"])
+def start_game():
+        subprocess.Popen(['python', 'new/1.py'])    
+
+if __name__ == "__main__":
+    skills_app.run(debug=True, port=9000)
